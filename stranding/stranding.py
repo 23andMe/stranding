8e98034134be1e5273dc031dcb3105ab3cea4445
Mat Strand <mstrand@23andme.com>
2017-03-03 01:39:50 -0800

    Refactor to GenomeStrander(...).strand_flanks(...)

--- stranding/stranding.py
+++ stranding/stranding.py
@@ -13,70 +13,87 @@ from .exceptions import (MissingReferenceFlank,
 logger = logging.getLogger("stranding")
 
 
-MIN_FLANK_LENGTH = 10
-DEFAULT_MAX_OFFSET = 20
-SCORE_MULTIPLIER = 1.5
-IDENTITY_CUTOFF_RATIO = 0.90
-MATCH_SCORE = 2
-MISMATCH_PENALTY = -1
-SSW_SCORING = swalign.NucleotideScoringMatrix(MATCH_SCORE, MISMATCH_PENALTY)
-ALIGNER = swalign.LocalAlignment(SSW_SCORING)
+DEFAULT_MIN_FLANK_LENGTH = 10
+DEFAULT_WINDOW_EXTENSION = 20
+DEFAULT_SCORE_MULTIPLIER = 1.5
+DEFAULT_IDENTITY_CUTOFF_RATIO = 0.90
+DEFAULT_MATCH_SCORE = 2
+DEFAULT_MISMATCH_PENALTY = -1
 
 
-def is_high_scoring(alignment):
-    return (alignment.identity > IDENTITY_CUTOFF_RATIO and
-            alignment.score > len(alignment.query) * SCORE_MULTIPLIER)
+class GenomeStrander(object):
+
+    def __init__(self,
+                 min_flank_length=DEFAULT_MIN_FLANK_LENGTH,
+                 score_multiplier=DEFAULT_SCORE_MULTIPLIER,
+                 identity_cutoff_ratio=DEFAULT_IDENTITY_CUTOFF_RATIO,
+                 match_score=DEFAULT_MATCH_SCORE,
+                 mismatch_penalty=DEFAULT_MISMATCH_PENALTY):
+
+            self.min_flank_length = min_flank_length
+            self.score_multiplier = score_multiplier
+            self.identity_cutoff_ratio = identity_cutoff_ratio
+            self.match_score = match_score
+            self.mismatch_penalty = mismatch_penalty
+            ssw_scoring = swalign.NucleotideScoringMatrix(self.match_score, self.mismatch_penalty)
+            self.aligner = swalign.LocalAlignment(ssw_scoring)
+
+    def is_high_scoring(self, alignment):
+        print self.identity_cutoff_ratio
+        print self.score_multiplier
+        return (alignment.identity > self.identity_cutoff_ratio and
+                alignment.score > len(alignment.query) * self.score_multiplier)
 
 
-def genome_stranding(_5p, _3p, build, chr_name, pos, max_offset=DEFAULT_MAX_OFFSET):
-    """
-    Given a 5' flank, a 3' flank, the assembly, chromosome, and position
-    determine whether a strand flip is required to get to forward genome stranding.
+    def strand_flanks(self, _5p, _3p, build, chr_name, pos, window=DEFAULT_WINDOW_EXTENSION):
+        """
+        Given a 5' flank, a 3' flank, the assembly, chromosome, and position
+        determine whether a strand flip is required to get to forward genome stranding.
 
-    A better way of doing this would be to integrate something like BLAT which can
-    quickly map arbitrary sequences in a genome assembly but has setup and licencing issues.
-    """
-    # sanity checks
-    if pos == 0:
-        raise Unstrandable('Position 0 is unmapped')
-    elif chr_name in ('0', 0):
-        raise Unstrandable('Chromosome 0 is unmapped')
-    elif max(_5p, _3p) < MIN_FLANK_LENGTH:
-        raise FlanksTooShort('Min flank length is specified as 10')
+        Consider BLAT if mapping information is unknown.
+        """
 
-    # chromosome-specific conventions
-    loop = chr_name == 'MT'
-    chr_name = chr_name if chr_name != 'XY' else 'X'
+        # sanity checks
+        if pos == 0:
+            raise Unstrandable('Position 0 is unmapped')
+        elif chr_name in ('0', 0):
+            raise Unstrandable('Chromosome 0 is unmapped')
+        elif max(_5p, _3p) < self.min_flank_length:
+            raise FlanksTooShort('Min flank length is specified as %d' % self.min_flank_length)
 
-    # reference sequences
-    try:
-        chromosome = Chromosome(chr_name, build, loop=loop)
-        ref_5p = chromosome.sequence(pos - max_offset - len(_5p), pos + max_offset)
-        ref_3p = chromosome.sequence(pos - max_offset, pos + len(_3p) + max_offset)
-    except ValueError:
-        raise MissingReferenceFlank()
+        # chromosome-specific conventions
+        loop = chr_name == 'MT'
+        chr_name = chr_name if chr_name != 'XY' else 'X'
 
-    # alignments named w.r.t the reference sequence
-    fwd_5p_alignment = ALIGNER.align(ref_5p, _5p)
-    fwd_3p_alignment = ALIGNER.align(ref_3p, _3p)
-    rev_5p_alignment = ALIGNER.align(swalign.revcomp(ref_5p), _3p, rc=True)
-    rev_3p_alignment = ALIGNER.align(swalign.revcomp(ref_3p), _5p, rc=True)
+        # reference sequences
+        try:
+            chromosome = Chromosome(chr_name, build, loop=loop)
+            ref_5p = chromosome.sequence(pos - window - len(_5p), pos + window)
+            ref_3p = chromosome.sequence(pos - window, pos + len(_3p) + window)
+        except ValueError:
+            raise MissingReferenceFlank()
 
-    alignments = [fwd_5p_alignment, fwd_3p_alignment, rev_5p_alignment, rev_3p_alignment]
-    high_scoring_alignments = [a for a in alignments if is_high_scoring(a)]
-    strands = [-1 if a.rc else 1 for a in high_scoring_alignments]
+        # alignments named w.r.t the reference sequence
+        fwd_5p_alignment = self.aligner.align(ref_5p, _5p)
+        fwd_3p_alignment = self.aligner.align(ref_3p, _3p)
+        rev_5p_alignment = self.aligner.align(swalign.revcomp(ref_5p), _3p, rc=True)
+        rev_3p_alignment = self.aligner.align(swalign.revcomp(ref_3p), _5p, rc=True)
 
-    for alignment in high_scoring_alignments:
-        logger.info(alignment.dump())
+        alignments = [fwd_5p_alignment, fwd_3p_alignment, rev_5p_alignment, rev_3p_alignment]
+        high_scoring_alignments = [a for a in alignments if self.is_high_scoring(a)]
+        strands = [-1 if a.rc else 1 for a in high_scoring_alignments]
 
-    if not high_scoring_alignments:
-        for alignment in alignments:
-            logger.debug(alignment.query)
-            logger.debug(alignment.ref)
-            logger.debug(alignment.dump())
-        raise Unstrandable()
+        for alignment in high_scoring_alignments:
+            logger.info(alignment.dump())
 
-    if not len(set(strands)) == 1:
-        raise InconsistentAlignment()
+        if not high_scoring_alignments:
+            for alignment in alignments:
+                logger.debug(alignment.query)
+                logger.debug(alignment.ref)
+                logger.debug(alignment.dump())
+            raise Unstrandable()
 
-    return strands[0]
+        if not len(set(strands)) == 1:
+            raise InconsistentAlignment()
+
+        return strands[0]
--- tests/test_functional.py
+++ tests/test_functional.py
@@ -16,6 +16,7 @@ hg38_seq = ('CCTCGGTGCTCTGGCCGAGCATGGCCTGCACCTCGCCGCGGTACTGCACCAGGCGGCCGCA'
 class SeqSeekTestCase(TestCase):
     def setUp(self):
         os.environ["SEQSEEK_DATA_DIR"] = os.path.join('tests', 'seqseek_fixtures')
+        self.strander = stranding.GenomeStrander()
 
 
 class TestBasicStranding(SeqSeekTestCase):
@@ -24,41 +25,41 @@ class TestBasicStranding(SeqSeekTestCase):
 
     def test_perfect_forward_alignment(self):
         _5p, _3p = self.PERFECT_5P, self.PERFECT_3P
-        assert 1 == stranding.genome_stranding(_5p, _3p, BUILD37, 1, 60, max_offset=10)
+        assert 1 == self.strander.strand_flanks(_5p, _3p, BUILD37, 1, 60)
 
     def test_perfect_reverse_alignment(self):
         _5p, _3p = self.PERFECT_5P, self.PERFECT_3P
-        assert -1 == stranding.genome_stranding(_5p, _3p, BUILD38, 1, 50, max_offset=10)
+        assert -1 == self.strander.strand_flanks(_5p, _3p, BUILD38, 1, 50, window=10)
 
     def test_offset_forward_alignment(self):
         _5p, _3p = self.PERFECT_5P, self.PERFECT_3P
-        assert 1 == stranding.genome_stranding(_5p, _3p, BUILD37, 1, 50)
+        assert 1 == self.strander.strand_flanks(_5p, _3p, BUILD37, 1, 50)
 
     def test_offset_reverse_alignment(self):
         _5p, _3p = self.PERFECT_5P, self.PERFECT_3P
-        assert -1 == stranding.genome_stranding(_5p, _3p, BUILD38, 1, 50)
+        assert -1 == self.strander.strand_flanks(_5p, _3p, BUILD38, 1, 50)
 
     def test_alignment_beyond_offset_boundary(self):
         _5p, _3p = self.PERFECT_5P, self.PERFECT_3P
         with self.assertRaises(stranding.Unstrandable):
-            stranding.genome_stranding(_5p, _3p, BUILD38, 1, 40, max_offset=10)
+            self.strander.strand_flanks(_5p, _3p, BUILD38, 1, 40, window=10)
 
     def test_alignment_too_close_to_contig_boundary(self):
         _5p, _3p = self.PERFECT_5P, self.PERFECT_3P
         with self.assertRaises(stranding.MissingReferenceFlank):
-            stranding.genome_stranding(_5p, _3p, BUILD38, 1, 10)
+            self.strander.strand_flanks(_5p, _3p, BUILD38, 1, 10)
 
     def test_chromosome_0(self):
         _5p, _3p = self.PERFECT_5P, self.PERFECT_3P
         try:
-            stranding.genome_stranding(_5p, _3p, BUILD37, 0, 60, max_offset=10)
+            self.strander.strand_flanks(_5p, _3p, BUILD37, 0, 60, window=10)
         except stranding.Unstrandable as e:
             assert str(e) == 'Chromosome 0 is unmapped'
 
     def test_position_0(self):
         _5p, _3p = self.PERFECT_5P, self.PERFECT_3P
         try:
-            stranding.genome_stranding(_5p, _3p, BUILD37, 1, 0, max_offset=10)
+            self.strander.strand_flanks(_5p, _3p, BUILD37, 1, 0, window=10)
         except stranding.Unstrandable as e:
             assert str(e) == 'Position 0 is unmapped'
 
@@ -68,11 +69,11 @@ class TestFuzzyStranding(SeqSeekTestCase):
     def test_fuzzy_stranding(self):
         _5p = 'GCGCAGGCCCGGCTGCGCGCGGTCATGGAGGACGTGT'
         _3p = 'tttttttttttttttttttttt'
-        assert 1 == stranding.genome_stranding(_5p, _3p, BUILD37, 1, 60, max_offset=10)
-        assert -1 == stranding.genome_stranding(_5p, _3p, BUILD38, 1, 60, max_offset=10)
+        assert 1 == self.strander.strand_flanks(_5p, _3p, BUILD37, 1, 60, window=10)
+        assert -1 == self.strander.strand_flanks(_5p, _3p, BUILD38, 1, 60, window=10)
 
     def test_inconsistent_stranding(self):
         _5p = "CGCCTG"  # this is present on both the forward and reverse strand in the same region
         with self.assertRaises(stranding.InconsistentAlignment):
-            stranding.genome_stranding(_5p, '', BUILD37, 1, 80)
+            self.strander.strand_flanks(_5p, '', BUILD37, 1, 80)
 
